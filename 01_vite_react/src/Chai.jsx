594769fd8f23bbf4f242b42ef5ed46b5b267bee5
Himanshu Kumar <himanshukr864@gmail.com>
2026-07-05 22:25:25 +0530

    add custom component

---
+++ 01_vite_react/src/Chai.jsx
@@ -0,0 +1,14 @@
+
+// Always write the function name with the first letter of the function name capital when making component or working with jsx
+// although the file name can start with small letter but keep that also capital to follow best practice and,
+//  put the extension name as jsx when working with vite.
+function Chai(params) {
+    return(
+        <h3>Chai is ready </h3>
+    )
+}
+
+export default Chai
+
+// now exporting or using this chai function in the App.jsx file
+
